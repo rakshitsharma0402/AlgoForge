6675f360ea21fdf2ff5ba6d55a40dc1b7c5516a1
Rakshit Sharma <kumarrakshit0402@gmail.com>
2026-07-19 18:11:59 +0530

    feat/cll-deletion : Implemented Deletion in Circular Linked List

--- src/main/java/org/algoforge/datastructures/list/CircularLinkedList.java
+++ src/main/java/org/algoforge/datastructures/list/CircularLinkedList.java
@@ -79,8 +79,42 @@ public class CircularLinkedList implements List {
     }
 
     public void deleteAtIndex(int index) {
+        if (index < 0 || size() <= index) {
+            throw new IndexOutOfBoundsException("Invalid position: " + index);
+        }
 
+        if (index == 0) {
+            deleteAtBeginning();
+            return;
+        }
+        if (index == size() - 1) {
+            deleteAtEnd();
+            return;
+        }
+
+        if (index <= size() / 2) {
+            Node curr = head;
+
+            for (int i = 0; i < index - 1; i++) {
+                curr = curr.next;
+            }
+
+            curr.next = curr.next.next;
+            curr.next.prev = curr;
+        } else {
+            Node curr = tail;
+
+            for (int i = size() - 1; i > index; i--) {
+                curr = curr.prev;
+            }
+
+            curr.prev.next = curr.next;
+            curr.next.prev = curr.prev;
+        }
+
+        size--;
     }
+
 
     @Override
     public String toString() {
